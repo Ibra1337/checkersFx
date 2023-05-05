0b148beea262e0fd863fa87ecd2666524073791a
ibra <ibrahim.joadou@gmail.com>
2023-05-06 01:12:58 +0200

    scom

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -1,9 +1,11 @@
 package Client;
 
 import GUI.CheckersBoardView;
+import Server.IMatchmaking;
 import resources.IPlayer;
 import resources.Player;
 
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -18,9 +20,12 @@ public class Client {
         try {
             Registry reg = LocateRegistry.getRegistry("192.168.220.1" , 1099);
             Player p = new Player(1);
-
+            IMatchmaking mm = (IMatchmaking) reg.lookup("mm");
+            mm.addPlayer(p);
         } catch (RemoteException e) {
             throw new RuntimeException(e);
+        } catch (NotBoundException e) {
+            e.printStackTrace();
         }
 
 
